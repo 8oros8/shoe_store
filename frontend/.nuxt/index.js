9be321f81955a6d7a8b7923b6600a0a95fea698f
Timur Karamyshev <timur0karamyshev@gmail.com>
2022-03-17 12:57:39 +0300

    Download nuxt files and node modules

--- frontend/.nuxt/index.js
+++ frontend/.nuxt/index.js
@@ -12,7 +12,7 @@ import { setContext, getLocation, getRouteData, normalizeError } from './utils'
 
 /* Plugins */
 
-import nuxt_plugin_plugin_497bc1c5 from 'nuxt_plugin_plugin_497bc1c5' // Source: ./components/plugin.js (mode: 'all')
+import nuxt_plugin_plugin_4fa7f1be from 'nuxt_plugin_plugin_4fa7f1be' // Source: ./components/plugin.js (mode: 'all')
 
 // Component: <ClientOnly>
 Vue.component(ClientOnly.name, ClientOnly)
@@ -176,8 +176,8 @@ async function createApp(ssrContext, config = {}) {
   }
   // Plugin execution
 
-  if (typeof nuxt_plugin_plugin_497bc1c5 === 'function') {
-    await nuxt_plugin_plugin_497bc1c5(app.context, inject)
+  if (typeof nuxt_plugin_plugin_4fa7f1be === 'function') {
+    await nuxt_plugin_plugin_4fa7f1be(app.context, inject)
   }
 
   // Lock enablePreview in context
--- frontend/.nuxt/router.js
+++ frontend/.nuxt/router.js
@@ -4,7 +4,7 @@ import { normalizeURL, decode } from 'ufo'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _a825223a = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+const _9d9a29f2 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 const emptyFn = () => {}
 
@@ -19,7 +19,7 @@ export const routerOptions = {
 
   routes: [{
     path: "/",
-    component: _a825223a,
+    component: _9d9a29f2,
     name: "index"
   }],
 
